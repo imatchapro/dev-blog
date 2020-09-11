f70b62fca41a5401d839f1da7ee9190174253b88
Yuta Takahashi <penityai@gmail.com>
2020-09-12 06:13:09 +0900

    add: Google Analytics settings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,22 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-113810755-4"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'UA-113810755-4');
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
