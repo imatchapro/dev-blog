fec487b24db77aa96eca6f5011908e0fe6626dc9
Yuta Takahashi <penityai@gmail.com>
2020-09-15 02:52:51 +0900

    fix: styled

--- src/components/molecules/ModalCookieAgreement.tsx
+++ src/components/molecules/ModalCookieAgreement.tsx
@@ -25,17 +25,17 @@ const ModalCookieAgreement: React.FC = () => {
 
   return (
     display && (
-      <div className="w-full fixed bottom-0 right-0 p-3">
+      <div className="w-full fixed bottom-0 right-0 p-3 sm:p-4">
         <div className="max-w-screen-md mx-auto rounded bg-gray-900 py-4 px-4 sm:px-6">
-          <p className="text-xs leading-loose sm:text-sm">
+          <p className="text-xs text-gray-300 leading-loose sm:text-sm">
             このサイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。サイトを利用することで、Cookieの使用に同意するものとします。
             <Link href={'/privacy'}>
-              <a className="text-blue-700">プライバシーポリシーはこちら</a>
+              <a className="text-blue-500">プライバシーポリシーはこちら</a>
             </Link>
           </p>
           <button
             onClick={handleClickCloseButton}
-            className="ml-auto mt-3 flex items-center flex-shrink-0 bg-gray-600 py-1 px-2 rounded text-gray-900 font-semibold text-xs"
+            className="ml-auto mt-3 flex items-center bg-gray-300 py-1 px-2 rounded text-gray-900 font-semibold text-xs sm:text-sm"
           >
             <FiX className="mr-1" />
             閉じる
