16bc9509ceea60edf9720539baea34f447e572e8
Yuta Takahashi <takahashi@101de-sign.com>
2020-11-18 01:29:13 +0900

    fix: Text correction and refactoring

--- src/components/organisms/TheContentsNavigation.tsx
+++ src/components/organisms/TheContentsNavigation.tsx
@@ -8,49 +8,47 @@ type Props = {
   rootClassName?: string;
 };
 
-const TheContentsNavigation: React.FC<Props> = ({ rootClassName }) => {
-  return (
-    <nav className={rootClassName}>
-      <ul className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:gap-8">
-        <li>
-          <ContentNavItemHeading href={'/blog'} text="Blog" icon={<FiPenTool />} />
-          <ContentNavItemText>
-            WEBの開発について（主にフロントエンド側）のブログを書いています。
-            <br />
-            自身の勉強や記録として記事を書いている場合がほとんどなので、間違いや不適切な表現があれば、ご連絡いただけると幸いです。
-          </ContentNavItemText>
-          <ContentNavItemLink href={'/blog'} text="記事の一覧を見る" />
-        </li>
-        <li>
-          <ContentNavItemHeading href={'/works'} text="Works" icon={<FiCode />} />
-          <ContentNavItemText>
-            個人で制作したアプリケーションやライブラリ、関わったプロダクトなどの実績を一覧で閲覧できるようにする予定です。
-            <br />
-            現在公開方法を考え中なので、追々追加していきます。
-          </ContentNavItemText>
-          <ContentNavItemLink href={'/works'} text="実績の一覧を見る" />
-        </li>
-        <li>
-          <ContentNavItemHeading href={'/contact'} text="Contact" icon={<FiMail />} />
-          <ContentNavItemText>
-            ブログの記事についての意見や質問、お仕事の依頼などのお問い合わせはこちらからお願いします。
-            <br />
-            お急ぎの場合は、
-            <a
-              className="text-blue-700 font-medium hover:text-blue-400 transition-navigation"
-              href="https://twitter.com/takayu_dev"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Twitter
-            </a>
-            のDMも開放しているので、お気軽にメッセージを送ってください。
-          </ContentNavItemText>
-          <ContentNavItemLink href={'/contact'} text="お問い合わせをする" />
-        </li>
-      </ul>
-    </nav>
-  );
-};
+const TheContentsNavigation: React.FC<Props> = ({ rootClassName }) => (
+  <nav className={rootClassName}>
+    <ul className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:gap-8">
+      <li>
+        <ContentNavItemHeading href={'/blog'} text="Blog" icon={<FiPenTool />} />
+        <ContentNavItemText>
+          WEBの開発について（主にフロントエンド側）のブログを書いています。
+          <br />
+          自身の勉強や記録として記事を書いている場合がほとんどなので、間違いや不適切な表現があればご連絡下さい。
+        </ContentNavItemText>
+        <ContentNavItemLink href={'/blog'} text="記事の一覧を見る" />
+      </li>
+      <li>
+        <ContentNavItemHeading href={'/works'} text="Works" icon={<FiCode />} />
+        <ContentNavItemText>
+          個人で制作したアプリケーションやライブラリ、関わったプロダクトなどの実績を一覧で閲覧できるようにする予定です。
+          <br />
+          現在公開方法を考え中なので、追々追加していきます。
+        </ContentNavItemText>
+        <ContentNavItemLink href={'/works'} text="実績の一覧を見る" />
+      </li>
+      <li>
+        <ContentNavItemHeading href={'/contact'} text="Contact" icon={<FiMail />} />
+        <ContentNavItemText>
+          ブログの記事についての意見や質問、お仕事の依頼などのお問い合わせはこちらからお願いします。
+          <br />
+          お急ぎの場合は、
+          <a
+            className="text-blue-700 font-medium hover:text-blue-400 transition-navigation"
+            href="https://twitter.com/takayu_dev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+          のDMも開放しているので、お気軽にメッセージを送ってください。
+        </ContentNavItemText>
+        <ContentNavItemLink href={'/contact'} text="お問い合わせをする" />
+      </li>
+    </ul>
+  </nav>
+);
 
 export default TheContentsNavigation;
--- src/components/organisms/TheProfile.tsx
+++ src/components/organisms/TheProfile.tsx
@@ -2,47 +2,45 @@ import React from 'react';
 import { FiGithub, FiTwitter, FiMapPin, FiMessageCircle } from 'react-icons/fi';
 import ProfileListItem from '../atoms/ProfileListItem';
 
-const TheProfile: React.FC = () => {
-  return (
-    <section className="mt-12 pt-12 border-t flex sm:mt-12 sm:pt-12">
-      <picture className="flex-shrink-0">
-        <source
-          className="w-20 h-20 rounded bg-gray-300 sm:w-32 sm:h-32"
-          type="image/webp"
-          srcSet="/profile.webp"
-        />
-        <img
-          className="w-20 h-20 rounded bg-gray-300 sm:w-32 sm:h-32"
-          src="/profile.jpg"
-          alt="Yuta Takahashi"
-          width="256"
-          height="256"
-          loading="lazy"
-        />
-      </picture>
-      <div className="ml-6 sm:ml-12">
-        <h1 className="text-2xl text-gray-900 font-semibold leading-none sm:text-3xl">
-          Yuta Takahashi
-        </h1>
-        <nav className="mt-4 sm:mt-6">
-          <ul>
-            <ProfileListItem icon={FiMessageCircle} text="ReactとTypeScriptとアイラモルトが好き" />
-            <ProfileListItem
-              icon={FiMapPin}
-              text="Fukuoka, Japan"
-              href="https://www.google.co.jp/maps/place/%E7%A6%8F%E5%B2%A1%E7%9C%8C%E7%A6%8F%E5%B2%A1%E5%B8%82/@33.6496589,129.9837839,10z/data=!3m1!4b1!4m5!3m4!1s0x3541eda1e9848429:0xf60a729936398783!8m2!3d33.5901838!4d130.4016888"
-            />
-            <ProfileListItem icon={FiGithub} text="@takayuu" href="https://github.com/takayuu" />
-            <ProfileListItem
-              icon={FiTwitter}
-              text="@takayu_dev"
-              href="https://twitter.com/takayu_dev"
-            />
-          </ul>
-        </nav>
-      </div>
-    </section>
-  );
-};
+const TheProfile: React.FC = () => (
+  <section className="mt-12 pt-12 border-t flex sm:mt-12 sm:pt-12">
+    <picture className="flex-shrink-0">
+      <source
+        className="w-20 h-20 rounded bg-gray-300 sm:w-32 sm:h-32"
+        type="image/webp"
+        srcSet="/profile.webp"
+      />
+      <img
+        className="w-20 h-20 rounded bg-gray-300 sm:w-32 sm:h-32"
+        src="/profile.jpg"
+        alt="Yuta Takahashi"
+        width="256"
+        height="256"
+        loading="lazy"
+      />
+    </picture>
+    <div className="ml-6 sm:ml-12">
+      <h1 className="text-2xl text-gray-900 font-semibold leading-none sm:text-3xl">
+        Yuta Takahashi
+      </h1>
+      <nav className="mt-4 sm:mt-6">
+        <ul>
+          <ProfileListItem icon={FiMessageCircle} text="ReactとTypeScriptとお酒が好き" />
+          <ProfileListItem
+            icon={FiMapPin}
+            text="Fukuoka, Japan"
+            href="https://www.google.co.jp/maps/place/%E7%A6%8F%E5%B2%A1%E7%9C%8C%E7%A6%8F%E5%B2%A1%E5%B8%82/@33.6496589,129.9837839,10z/data=!3m1!4b1!4m5!3m4!1s0x3541eda1e9848429:0xf60a729936398783!8m2!3d33.5901838!4d130.4016888"
+          />
+          <ProfileListItem icon={FiGithub} text="@takayuu" href="https://github.com/takayuu" />
+          <ProfileListItem
+            icon={FiTwitter}
+            text="@takayu_dev"
+            href="https://twitter.com/takayu_dev"
+          />
+        </ul>
+      </nav>
+    </div>
+  </section>
+);
 
 export default TheProfile;
