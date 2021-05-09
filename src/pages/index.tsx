c01e6ca2857d05122aaa1498aacc12a075ad0517
Yuta Takahashi <takahashi@101de-sign.com>
2021-05-09 14:38:27 +0900

    refactor: Unified next-seo URL settings to _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,20 +4,17 @@ import { NextSeo } from 'next-seo';
 import TheMainView from '../components/organisms/TheMainView';
 import TheContentsNavigation from '../components/organisms/TheContentsNavigation';
 import TheProfile from '../components/organisms/TheProfile';
-import useAbsoluteUrl from '../hooks/useAbsoluteUrl';
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <NextSeo
-        title="Home"
-        description="takayu.devは、主にフロントエンドを中心とした開発ブログや、個人で制作したアプリケーション、ライブラリ、関わったプロダクトの一覧をまとめているサイトです。"
-      />
-      <TheMainView />
-      <TheContentsNavigation rootClassName="mt-12" />
-      <TheProfile />
-    </>
-  );
-};
+const Home: NextPage = () => (
+  <>
+    <NextSeo
+      title="Home"
+      description="takayu.devは、主にフロントエンドを中心とした開発ブログや、個人で制作したアプリケーション、ライブラリ、関わったプロダクトの一覧をまとめているサイトです。"
+    />
+    <TheMainView />
+    <TheContentsNavigation rootClassName="mt-12" />
+    <TheProfile />
+  </>
+);
 
 export default Home;
