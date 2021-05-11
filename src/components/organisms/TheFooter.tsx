6c14e7b97317f8681e79c79ed2235a9b4849c6e7
Yuta Takahashi <takahashi@101de-sign.com>
2021-05-12 01:56:36 +0900

    feat: Renewal TheHeader.tsx and TheFooter.tsx

--- src/components/organisms/TheFooter.tsx
+++ src/components/organisms/TheFooter.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Box, Flex, HStack, Icon, Link, Spacer, Text } from '@chakra-ui/react';
+import { FiGithub, FiTwitter } from 'react-icons/fi';
+import { IconType } from 'react-icons';
 
 const TheFooter: React.FC = () => {
   return (
-    <footer className="flex-shrink-0 bg-gray-900 px-6 py-4 sm:px-12 sm:py-6">
-      <small className="block text-center text-xs text-gray-300 sm:text-sm">
-        © 2020 TAKAYU.DEV
-      </small>
-    </footer>
+    <Box as="footer" borderTopWidth={1} borderTopColor="gray.200" borderTopStyle="solid">
+      <Flex maxW="container.xl" alignItems="center" mx="auto" h={20}>
+        <HStack spacing={8}>
+          <MenuServiceIcon to={process.env.NEXT_PUBLIC_GITHUB_URL} icon={FiGithub} />
+          <MenuServiceIcon to={process.env.NEXT_PUBLIC_TWITTER} icon={FiTwitter} />
+        </HStack>
+        <Spacer />
+        <Text as="small" fontSize="xs">
+          &copy; 2020 TAKAYU.DEV
+        </Text>
+      </Flex>
+    </Box>
   );
 };
+
+const MenuServiceIcon: React.VFC<{ to: string; icon: IconType }> = ({ to, icon }) => (
+  <Link color="gray.600" _hover={{ color: 'gray.800' }} href={to} isExternal>
+    <Icon as={icon} boxSize={6} />
+  </Link>
+);
 
 export default TheFooter;
--- src/components/organisms/TheHeader.tsx
+++ src/components/organisms/TheHeader.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
-import HeaderNavigation from '../molecules/HeaderNavigation';
-import HeaderLogo from '../atoms/HeaderLogo';
-import ButtonOpenMobileNav from '../atoms/ButtonOpenMobileNav';
+import NextLink from 'next/link';
+import { Box, Button, Flex, HStack, Icon, Spacer, Text } from '@chakra-ui/react';
+import { FiMoon } from 'react-icons/fi';
 
-const TheHeader: React.FC = () => {
+const TheHeader: React.VFC = () => {
   return (
-    <header className="header">
-      <HeaderLogo>TAKAYU.DEV</HeaderLogo>
-      <HeaderNavigation />
-      <ButtonOpenMobileNav />
-    </header>
+    <Box
+      as="header"
+      w="100%"
+      position="absolute"
+      borderBottomWidth={1}
+      borderBottomColor="gray.200"
+      borderBottomStyle="solid"
+    >
+      <Flex maxW="container.xl" h={20} alignItems="center" mx="auto">
+        <Logo />
+        <Spacer />
+        <HStack spacing={6}>
+          <HStack spacing={4}>
+            <MenuItem to="/blog">Blog</MenuItem>
+            <MenuItem to="/works">Works</MenuItem>
+          </HStack>
+          <Button size="sm" variant="ghost" px={0}>
+            <Icon as={FiMoon} boxSize={5} />
+          </Button>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+const Logo: React.VFC = () => (
+  <Box as="h1">
+    <NextLink href="/" passHref>
+      <Text as="a" fontSize="lg" fontWeight="bold">
+        {process.env.NEXT_PUBLIC_SITE_NAME}
+      </Text>
+    </NextLink>
+  </Box>
+);
+
+const MenuItem: React.FC<{ to: string }> = ({ to, children }) => {
+  return (
+    <Box p={2}>
+      <NextLink href={to} passHref>
+        <Text as="a" color="gray.600" fontSize="sm" fontWeight={600}>
+          {children}
+        </Text>
+      </NextLink>
+    </Box>
   );
 };
 
